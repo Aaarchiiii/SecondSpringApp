54d9589277b6758f3d790625b52ec6ca1be6fc9f
Aaarchiiii <archiagarwal23@gmail.com>
2025-03-01 22:05:34 +0530

    added

---
+++ src/test/java/com/archi/SecondSpringApp/SecondSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archi.SecondSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
